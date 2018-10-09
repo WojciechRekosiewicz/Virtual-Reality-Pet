a684f88a069407791fb2281b12b0efd74711ce98
WojciechRekosiewicz <wojtek24a@gmail.com>
2018-10-09 15:53:58 +0200

    another test added

--- src/Model/Statistics.java
+++ src/Model/Statistics.java
@@ -1,4 +1,25 @@
 package Model;
 
+import java.util.Random;
+
 public class Statistics {
+    Random generator = new Random();
+    int strength = generator.nextInt(100);
+    int dexterity = generator.nextInt(100);
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
 }
--- test/test.java
+++ test/test.java
@@ -1,4 +1,5 @@
 import Model.ActivityLvl;
+import Model.Statistics;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -6,14 +7,22 @@ import java.util.Random;
 
 public class test {
     @Test
-    public void ifStatsHaveBeenCreated() {
+    public void isStatsHaveBeenCreated() {
         ActivityLvl AL = new ActivityLvl();
-        Random generator = new Random();
-        int strength = generator.nextInt(100);
-        int dexterity = generator.nextInt(100);
         int feedLvl = 50;
         int funLvl = 50;
         Assert.assertEquals(feedLvl, AL.getFeedLvl());
         Assert.assertEquals(funLvl, AL.getFunLvl());
+    }
+
+    @Test
+    public void isStrAndDexCreated() {
+        Statistics StatisticsObject = new Statistics();
+        Random generator = new Random();
+        int strength = generator.nextInt(100);
+        int dexterity = generator.nextInt(100);
+        Assert.assertNotEquals(strength, StatisticsObject.getStrength());
+        Assert.assertNotEquals(strength, StatisticsObject.getStrength());
+
     }
 }
